a15aba8dca4cdc6b01388af8fea60733ffaf0aad
Robin <robin.thomas@edhec.com>
2025-10-15 13:47:45 +0200

    Final python module update

--- RobinToolKit.py
+++ RobinToolKit.py
@@ -717,3 +717,56 @@ def shrinkage_cov(r, delta=0.5, **kwargs):
     sample = sample_cov(r, **kwargs)
     return delta*prior + (1-delta)*sample
  
+
+def risk_contribution(w,cov):
+    """
+    Compute the contributions to risk of the constituents of a portfolio, given a set of portfolio weights and a covariance matrix
+    """
+    total_portfolio_var = Portfolio_volatility(w,cov)**2
+    # Marginal contribution of each constituent
+    marginal_contrib = cov@w
+    risk_contrib = np.multiply(marginal_contrib,w.T)/total_portfolio_var
+    return risk_contrib
+
+def target_risk_contributions(target_risk, cov):
+    """
+    Returns the weights of the portfolio that gives you the weights such
+    that the contributions to portfolio risk are as close as possible to
+    the target_risk, given the covariance matrix
+    """
+    n = cov.shape[0]
+    init_guess = np.repeat(1/n, n)
+    bounds = ((0.0, 1.0),) * n # an N-tuple of 2-tuples!
+    # construct the constraints
+    weights_sum_to_1 = {'type': 'eq',
+                        'fun': lambda weights: np.sum(weights) - 1
+    }
+    def msd_risk(weights, target_risk, cov):
+        """
+        Returns the Mean Squared Difference in risk contributions
+        between weights and target_risk
+        """
+        w_contribs = risk_contribution(weights, cov)
+        return ((w_contribs-target_risk)**2).sum()
+    
+    weights = minimize(msd_risk, init_guess,
+                       args=(target_risk, cov), method='SLSQP',
+                       options={'disp': False},
+                       constraints=(weights_sum_to_1,),
+                       bounds=bounds)
+    return weights.x
+
+def equal_risk_contributions(cov):
+    """
+    Returns the weights of the portfolio that equalizes the contributions
+    of the constituents based on the given covariance matrix
+    """
+    n = cov.shape[0]
+    return target_risk_contributions(target_risk=np.repeat(1/n,n), cov=cov)
+
+def weight_erc(r, cov_estimator=sample_cov, **kwargs):
+    """
+    Produces the weights of the ERC portfolio given a covariance matrix of the returns 
+    """
+    est_cov = cov_estimator(r, **kwargs)
+    return equal_risk_contributions(est_cov)
